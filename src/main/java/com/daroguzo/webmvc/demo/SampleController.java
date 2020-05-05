2605241f2d3f1e8abc99fdc8ed05a1b2d930502f
김진우 <kjw2122@naver.com>
2020-05-05 17:49:54 +0900

    미디어 타입에 따른 요청 매핑하기

--- src/main/java/com/daroguzo/webmvc/demo/SampleController.java
+++ src/main/java/com/daroguzo/webmvc/demo/SampleController.java
@@ -1,5 +1,6 @@
 package com.daroguzo.webmvc.demo;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(method = RequestMethod.GET)
 public class SampleController {
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public String hello(){
         return "hello";
--- src/test/java/com/daroguzo/webmvc/demo/SampleControllerTest.java
+++ src/test/java/com/daroguzo/webmvc/demo/SampleControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,12 +23,10 @@ public class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception {
-        mockMvc.perform(get("/hello"))
-                .andDo(print())
-                .andExpect(status().isOk())
-        ;
-
-        mockMvc.perform(get("/hi"))
+        mockMvc.perform(get("/hello")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+        )
                 .andDo(print())
                 .andExpect(status().isOk())
         ;
