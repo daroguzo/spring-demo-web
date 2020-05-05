387cadf1529ce7a756d0d8f78d8b20ca306b72c7
김진우 <kjw2122@naver.com>
2020-05-05 17:23:11 +0900

    Initial commit

---
+++ src/main/java/com/daroguzo/webmvc/demo/SampleController.java
@@ -0,0 +1,19 @@
+package com.daroguzo.webmvc.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(method = RequestMethod.GET)
+public class SampleController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
+
+}
