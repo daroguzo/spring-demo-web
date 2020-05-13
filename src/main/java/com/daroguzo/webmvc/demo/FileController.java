96f84a64eb7e637bfb704839d3d3baf7aa60bbdb
김진우 <kjw2122@naver.com>
2020-05-13 19:49:37 +0900

    핸들러 메소드: ResponseEntity를 통한 파일 다운로드

--- src/main/java/com/daroguzo/webmvc/demo/FileController.java
+++ src/main/java/com/daroguzo/webmvc/demo/FileController.java
@@ -1,15 +1,25 @@
 package com.daroguzo.webmvc.demo;
 
+import org.apache.tika.Tika;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 public class FileController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @GetMapping("/file")
     public String fileUploadForm(Model model){
@@ -25,4 +35,15 @@ public class FileController {
         return "redirect:/file";
     }
 
+    @GetMapping("/file/{filename}")
+    @ResponseBody
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + filename);
+        File file = resource.getFile();
+        Tika tika = new Tika();
+        String mediaType = tika.detect(file);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachement; filename=\"" + resource.getFilename() + "\"")
+                .header(HttpHeaders.CONTENT_TYPE, mediaType) .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length())) .body(resource);
+    }
 }
